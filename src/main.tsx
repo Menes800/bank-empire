61ee6580113eb3531d8b2b7fe1bedeec3f2410af
Menes800 <r.menes89@gmail.com>
2026-07-20 11:14:42 +0200

    Load v0.4.1 refinement styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './v3.css';
 import './v4.css';
+import './v41.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
