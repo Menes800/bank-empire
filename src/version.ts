03eec8d40794cb09c3df3ccd3316ce44fa414579
Menes800 <r.menes89@gmail.com>
2026-07-21 11:24:02 +0200

    Release v0.8.7 economy integrity

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "0.8.6";
-export const APP_RELEASE_NAME = "Management & Delegation";
+export const APP_VERSION = "0.8.7";
+export const APP_RELEASE_NAME = "Economy & Data Integrity";
