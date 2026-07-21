37e807c68058bede1c0e44049e7301a5204aae88
Menes800 <r.menes89@gmail.com>
2026-07-21 09:36:06 +0200

    Set v0.8.6 Management Delegation release

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "0.8.5";
-export const APP_RELEASE_NAME = "Leadership & Delegation";
+export const APP_VERSION = "0.8.6";
+export const APP_RELEASE_NAME = "Management & Delegation";
