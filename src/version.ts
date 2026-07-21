0afff44e9127b68a4b898988b8c922527eed7399
Menes800 <r.menes89@gmail.com>
2026-07-21 15:33:44 +0200

    Release v0.8.10 clarity and connected systems

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "0.8.9.2";
-export const APP_RELEASE_NAME = "Split-screen Layout Fix";
+export const APP_VERSION = "0.8.10";
+export const APP_RELEASE_NAME = "Clarity & Connected Systems";
