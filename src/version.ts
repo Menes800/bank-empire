fb3af938e7fcd705d98ff213ab8284b4ddc89540
Menes800 <r.menes89@gmail.com>
2026-07-21 14:16:12 +0200

    Set v0.8.9 release identity

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "0.8.8";
-export const APP_RELEASE_NAME = "Delegation, Identity & Progression";
+export const APP_VERSION = "0.8.9";
+export const APP_RELEASE_NAME = "Connected Management & UI";
